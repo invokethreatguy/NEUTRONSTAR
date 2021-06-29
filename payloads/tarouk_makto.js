3eb90f907b2335547eccb6fe53df4c33a28152f1
secdev-01 <83469949+secdev-01@users.noreply.github.com>
2021-06-29 08:53:20 -0600

    Create tarouk_makto.js

---
+++ payloads/tarouk_makto.js
@@ -0,0 +1,4 @@
+// Novel JS Browser Custom Protocol Scheme / Handler.
+// "The way I had it figured, toruk is the baddest cat in the sky. Nothing attacks him. So why would he ever look up?" 
+
+[TODO] Ensuure this is clear to release.
